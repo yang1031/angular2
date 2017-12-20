bd589ca410a21ae97bc6df4a3a9d959c90abf556
yang1031 <553661679@qq.com>
2017-12-20 21:55:27 +0800

    add .gitignore

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from "@angular/router";
 
+import { rootRouterConfig } from "./app.routes";
 import { AppComponent } from './app.component';
+import { FooterComponent } from './common/footer/footer.component';
+import { IndexComponent } from './main/index/index.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    IndexComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(rootRouterConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,14 @@
 import { Routes } from "@angular/router";
 import { AppComponent } from './app.component';
-
+import { IndexComponent } from './main/index/index.component';
 export const rootRouterConfig: Routes =  [
   {
     path: "",
-    redirectTo: "",
-    pathMatch: AppComponent
+    redirectTo: "index",
+    pathMatch: 'full'
+  },
+  {
+    path: "index",
+    component: IndexComponent
   }
 ]
