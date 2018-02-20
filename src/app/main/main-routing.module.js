682a690ed194edf78eb2b92000a7fec1244db2c5
yang1031 <553661679@qq.com>
2018-02-20 16:22:41 +0800

    add index pages

--- src/app/main/main-routing.module.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var index_component_1 = require("./index/index.component");
-exports.routing = [
-    {
-        path: 'index',
-        component: index_component_1.IndexComponent
-    }
-];
